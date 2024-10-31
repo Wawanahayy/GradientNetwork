d71cdeba7a2b40d7b62bc2b4986265dc8673207f
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2024-10-31 08:22:39 +0700

    Update grad.py

--- grad.py
+++ grad.py
@@ -15,7 +15,7 @@ def unzip_crx(crx_path, extract_to_folder):
 
 
 crx_path = os.path.join(os.getcwd(), '1.0.1_0.crx')
-extract_to_folder = os.path.join(os.getcwd(), 'extension_folder')
+extract_to_folder = os.path.join(os.getcwd(), 'folder_ekstensi')
 
 if not os.path.exists(extract_to_folder):
     os.makedirs(extract_to_folder)
@@ -24,16 +24,16 @@ unzip_crx(crx_path, extract_to_folder)
 
 
 def create_extension_folder(proxy_host, proxy_port, proxy_user, proxy_pass):
-    extension_folder1 = os.path.join(os.getcwd(), 'extension_folder1')
+    folder_ekstensi1 = os.path.join(os.getcwd(), 'folder_ekstensi1')
 
-    if not os.path.exists(extension_folder1):
-        os.makedirs(extension_folder1)
+    if not os.path.exists(folder_ekstensi1):
+        os.makedirs(folder_ekstensi1)
 
     manifest_json = """
     {
         "version": "1.0.0",
         "manifest_version": 2,
-        "name": "Chrome Proxy",
+        "name": "Proxy Chrome",
         "permissions": [
             "proxy",
             "tabs",
@@ -78,29 +78,31 @@ def create_extension_folder(proxy_host, proxy_port, proxy_user, proxy_pass):
     );
     """ % (proxy_host, proxy_port, proxy_user, proxy_pass)
 
-    with open(os.path.join(extension_folder1, 'manifest.json'), 'w') as f:
+    with open(os.path.join(folder_ekstensi1, 'manifest.json'), 'w') as f:
         f.write(manifest_json)
 
-    with open(os.path.join(extension_folder1, 'background.js'), 'w') as f:
+    with open(os.path.join(folder_ekstensi1, 'background.js'), 'w') as f:
         f.write(background_js)
 
-    return extension_folder1
+    return folder_ekstensi1
 
 
-def launch_browser_with_proxy(proxy_host, proxy_port, proxy_user, proxy_pass):
+def launch_browser_with_proxy(proxy_host, proxy_port, proxy_user, proxy_pass, username, password):
     options = Options()
-    extension_folder_path = os.path.join(os.getcwd(), 'extension_folder')
-    extension_folder_path1 = create_extension_folder(proxy_host, proxy_port, proxy_user, proxy_pass)
+    folder_ekstensi_path = os.path.join(os.getcwd(), 'folder_ekstensi')
+    folder_ekstensi_path1 = create_extension_folder(proxy_host, proxy_port, proxy_user, proxy_pass)
+    
     if use_proxy == '1':
-        extensions = f"{extension_folder_path},{extension_folder_path1}"
+        extensions = f"{folder_ekstensi_path},{folder_ekstensi_path1}"
     else:
-        extensions = f"{extension_folder_path}"
+        extensions = f"{folder_ekstensi_path}"
 
     options.add_argument(f'--load-extension={extensions}')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-notifications")
     options.add_argument('--headless')
+    
     driver = uc.Chrome(options=options)
     time.sleep(5)
     driver.switch_to.window(driver.window_handles[0])
@@ -116,23 +118,28 @@ def launch_browser_with_proxy(proxy_host, proxy_port, proxy_user, proxy_pass):
     driver.switch_to.window(main_tab)
     wait = WebDriverWait(driver, 10)
 
+    # Menggunakan username dan password dari input pengguna
     email_input = wait.until(
         EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/div/div/div[2]/div[1]/input')))
-    email_input.send_keys("ЛОГИН")
+    email_input.send_keys(username)
 
     password_input = wait.until(
         EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/div/div/div[2]/div[2]/span/input')))
-    password_input.send_keys("ПАРОЛЬ")
+    password_input.send_keys(password)
 
     login_button = wait.until(
         EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div/div/div/div[4]/button[1]')))
     login_button.click()
-    print("Запустил фарм")
+    print("Farming dimulai")
     return driver
 
 
-num_browsers = int(input("Сколько браузеров открыть?\n"))
-use_proxy = input("Использовать прокси? (1 - Да, 2 - Нет)\n")
+num_browsers = int(input("Berapa banyak browser yang akan dibuka?\n"))
+use_proxy = input("Gunakan proxy? (1 - Ya, 2 - Tidak)\n")
+
+# Mengambil username dan password
+username = input("Masukkan username: ")
+password = input("Masukkan password: ")
 
 with open('proxies.txt', 'r') as proxy_file:
     proxies = proxy_file.read().strip().split('\n')
@@ -146,10 +153,10 @@ for i in range(min(num_browsers, len(proxies))):
         proxy_pass = match.group(2)
         proxy_host = match.group(3)
         proxy_port = match.group(4)
-        driver = launch_browser_with_proxy(proxy_host, proxy_port, proxy_user, proxy_pass)
+        driver = launch_browser_with_proxy(proxy_host, proxy_port, proxy_user, proxy_pass, username, password)
         drivers.append(driver)
 
-input("Нажмите Enter, чтобы закрыть все браузеры")
+input("Tekan Enter untuk menutup semua browser")
 
 for driver in drivers:
     driver.quit()
